433f1de4555e6e9b474966999619b011cd3bbb09
daeho sim <tlaeogh5@gmail.com>
2022-11-07 15:14:33 +0900

    second commit

--- operator.js
+++ operator.js
@@ -1,6 +1,7 @@
 const book = 10000
-const phone = 20000
+const phone = 10000
+const mirror = 10000
 
-let bkph = (book + phone)
+let bkph = (book + phone + mirror)
 
 console.log(`총 ${bkph * 0.8}에 물건을 구입합니다.`)
